63db3a19a238627e06ea46f396df2d829022a747
AlySkittles <alyssha.lew@gmail.com>
2022-04-05 21:42:02 -0500

    8-kyu solution commit

---
+++ Codewars-JS/8-kyu/Beginner-Lost-Without-a-Map/Beginner-Lost-Without-a-Map.js
@@ -0,0 +1,13 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+*/
+
+//My Solution
+
+function maps(x){
+    return x.map(n => n * 2);
+}
