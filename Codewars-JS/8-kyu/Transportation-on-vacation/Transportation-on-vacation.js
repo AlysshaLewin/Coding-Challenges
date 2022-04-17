9538f5b7c503f79e29a1f7b61839550228f9d7de
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-04-17 17:38:52 -0500

    Commit of Codewars Solution on 4/17/22

---
+++ Codewars-JS/8-kyu/Transportation-on-vacation/Transportation-on-vacation.js
@@ -0,0 +1,30 @@
+/*
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+*/
+
+//My Solution
+
+
+// car costs $40
+// 7 or more days, $50 off
+// 3 or more, $20 off
+
+function rentalCarCost(d) {
+    //  car costs $40
+    // 7 or more days, $50 off
+    if(d >= 7) {
+      return d*40 - 50
+    // 3 or more days, $20 off
+    } else if(d >= 3){
+      return d*40 - 20
+    // less than 3 days, no discount
+    } else {
+      return d*40
+    }
+  }
