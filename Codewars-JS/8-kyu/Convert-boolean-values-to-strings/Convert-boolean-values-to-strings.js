89daa2e1ad06077056dce90d8cb29a8dcec313c5
AlySkittles <alyssha.lew@gmail.com>
2022-05-09 10:19:02 -0500

    Commit of Codewars Solution on 5/9/22

---
+++ Codewars-JS/8-kyu/Convert-boolean-values-to-strings/Convert-boolean-values-to-strings.js
@@ -0,0 +1,10 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+//My Solution
+
+function boolToWord( bool ){
+    //ternary operator because if/else.. statement
+    return (bool ? 'Yes' : 'No');
+}
