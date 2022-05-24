a2da0452dd1b862a31dc709722d6d2d3157e2033
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-05-23 22:00:15 -0500

    Commit of Codewars Solution on 5/23/22

---
+++ Codewars-JS/8-kyu/Volume-of-a-Cuboid/Volume-of-a-Cuboid.js
@@ -0,0 +1,32 @@
+/*
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+
+*/
+
+//My Solution
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height;
+    }
+}
+
+
+
+/*What is PREP?
+
+PREP is a methodology in which to approach an algorithm or coding challenge. The acronym stands for Parameters, Return, Example, Pseudocode.
+
+    Parameters
+    What is the type of argument being inputted and are there multiple or default arguments?
+
+    Return
+    What is being asked to be returned and what is the type?
+
+    Example
+    Provide 2-3 examples of what we expect to happen if we call the function with certain arguments. Can you think of any edge cases?
+
+    Pseudocode
+    Write informal steps on how to solve the problem. Think about what the parameters are and what steps need to be taken to get to our expected return. All statements showing "dependency" are to be indented.
+*/
