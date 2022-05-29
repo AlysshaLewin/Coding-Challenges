8791044ec1c57dbe31cddc24b467d110789f50df
Alyssha Lewin <alyssha.lew@gmail.com>
2022-05-28 21:38:02 -0500

    Commit of Codewars Solution on 5-28-22

---
+++ Codewars-JS/8-kyu/Is-it-a-palindrome/Is-it-a-palindrome.js
@@ -0,0 +1,22 @@
+/*
+Write a function that checks if a given string (case insensitive) is a palindrome.
+
+
+*/
+
+//My Solution 
+
+let isPalindrome = (x) => x.toLowerCase().split('').reverse().join('') == x.toLowerCase() ? true : false 
+
+
+/*
+P: parameter of x
+
+R: checks are given string
+
+E: n/a
+
+P: 
+
+*/
+
