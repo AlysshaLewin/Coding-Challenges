5aa84ed3da0a20a51e37ec7f202296591cc65f7e
Alyssha Lewin <alyssha.lew@gmail.com>
2022-06-01 11:08:56 -0500

    Codewars Solution for 6/1/22

---
+++ Codewars-JS/8-kyu/Will-there-be-enough-space/Will-there-be-enough-space.js
@@ -0,0 +1,46 @@
+/*
+The Story:
+Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+Task Overview:
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus excluding the driver.
+wait is the number of people waiting to get on to the bus excluding the driver.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+Usage Examples:
+cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
+cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+*/
+
+//My Solution
+
+function enough(cap, on, wait) {
+    let remainingRoom = cap - on;  // available space left on bus
+    if(remainingRoom >= wait){
+        return 0;
+    } else if (remainingRoom < wait){
+        return (on + wait) - cap;
+    }
+}
+
+
+
+/*
+P: //cap is the amount of people the bus can hold excluding the driver.
+   //on is the number of people on the bus excluding the driver.
+   //wait is the number of people waiting to get on to the bus excluding the driver. 
+
+R: //return 0, and if there isn't, return the number of passengers he can't take
+
+E: //cap = 10, on = 5, wait = 5 --> 0 # He can fit all passengers
+   //cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
+
+P: 1 - create a variable that holds value for cap - on
+   2 - create an if statement for how many are waiting and return 0
+   3 - create and else if variable less than how many are waiting
+   4 - return on + wait - cap
+
+*/
