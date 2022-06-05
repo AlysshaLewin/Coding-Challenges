933e655b49a22a5dc975a2caf33a1ab79e36a387
Alyssha Lewin <alyssha.lew@gmail.com>
2022-06-04 19:02:01 -0500

    Solution for 6/4/22

---
+++ Codewars-JS/8-kyu/You-Cant-Code-Under-Pressure1/You-Cant-Code-Under-Pressure1.js
@@ -0,0 +1,17 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//My Solution
+
+function doubleInteger(i) {
+    return 2 * i;
+}
+
+/*
+P: param = i 
+
+R: return the integer after doubling it
+
+E: n/a
+
+P: multiply i by 2
+*/
