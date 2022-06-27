b681ac7834c817096e52fe595facfb1bd8867944
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-06-26 19:36:03 -0500

    Solution for 6/26/22

---
+++ Codewars-JS/8-kyu/Grasshopper-Debug-sayHello/Grasshopper-Debug-sayHello.js
@@ -0,0 +1,26 @@
+/*
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+*/
+
+//My Solution
+
+function sayHello(name) {
+    return `Hello, ${name}`;
+};
+
+/*
+P: param of names
+
+R: create a program to greet eveyone as they come abroad the starship Enterprise
+
+E: Hello, Mr. Spock
+Hello, Eddie
+Hello, Rachel
+
+P: return  a template literal of `Hello, ${name}`. 
+*/ 
