abcb2e6dff78496220979b154cfec1b7b270ca81
Alyssha Lewin <alyssha.lew@gmail.com>
2022-07-28 11:38:50 -0500

    Solution for 7/28/22

---
+++ Codewars-JS/8-kyu/Well-of-Ideas - Easy-Version/Well-of-Ideas - Easy-Version.js
@@ -0,0 +1,11 @@
+/*
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+//My Solution
+
+function well(x){
+    const count = x.reduce((s, v) => s + (v == 'good'), 0);
+    return count ? count > 2 ? 'I smell a series!' : 'Publish!' : 'Fail!';
+}
