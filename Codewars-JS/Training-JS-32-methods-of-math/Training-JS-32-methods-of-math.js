667aebab21a8351d638963f94afc1d2e2f4c097e
Alyssha Lewin <alyssha.lew@gmail.com>
2022-08-02 08:40:14 -0500

    Solution for 8/2/22

---
+++ Codewars-JS/Training-JS-32-methods-of-math/Training-JS-32-methods-of-math.js
@@ -0,0 +1,25 @@
+/*
+Coding in function roundIt. function accept 1 parameter n. It's a number with a decimal point. Please use different methods based on the location of the decimal point, turn the number into an integer.
+
+If the decimal point is on the left side of the number (that is, the count of digits on the left of the decimal point is less than that on the right), Using ceil() method.
+
+roundIt(3.45) should return 4
+If the decimal point is on the right side of the number (that is, the count of digits on the left of the decimal point is more than that on the right), Using floor() method.
+
+roundIt(34.5) should return 34
+If the decimal point is on the middle of the number (that is, the count of digits on the left of the decimal point is equals that on the right), Using round() method.
+
+roundIt(34.56) should return 35
+
+*/
+
+//My Solution
+
+function roundIt(n){
+    let [left, right] = n.toString().split('.').map(x => x.length),
+        dx = left - right,
+        fn = dx < 0 ? Math.ceil : dx > 0 ? Math.floor : Math.round
+        
+    return fn(n)
+    
+}
