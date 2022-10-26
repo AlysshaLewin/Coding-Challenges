231e0854bbb491e2ee6ee5506edebd565fce951f
Alyssha Lewin <alyssha.lew@gmail.com>
2022-10-26 09:48:43 -0500

    Solution for 10/26/22

---
+++ Codewars-JS/7-kyu/Numbers-to-Objects/Numbers-to-Objects.js
@@ -0,0 +1,22 @@
+/*
+You will be given an array of numbers.
+
+For each number in the array you will need to create an object.
+
+The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+
+Return an array of the resulting objects.
+
+All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
+*/
+
+//My Solution
+
+function numObj(s){
+    let obj = [];
+    for( let i = 0; i < s.length; i++){
+      obj[i] = {};
+      obj[i][s[i]] = String.fromCharCode(s[i]);
+    }
+    return obj;
+}
