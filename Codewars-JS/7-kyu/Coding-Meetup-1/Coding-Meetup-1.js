2ef5d657e87b0b5f437667a852ecefa87a8f8df8
Alyssha <90856589+AlysshaLewin@users.noreply.github.com>
2022-12-19 13:52:31 -0600

    Solution for 12/19/22

---
+++ Codewars-JS/7-kyu/Coding-Meetup-1/Coding-Meetup-1.js
@@ -0,0 +1,45 @@
+/*
+You will be given an array of objects (hashes in ruby) representing data about developers who have signed up to attend the coding meetup that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+your function should return number 1.
+
+If, there are no JavaScript developers from Europe then your function should return 0.
+
+Notes:
+
+The format of the strings will always be Europe and JavaScript.
+All data will always be valid and uniform as in the example above.
+
+*/
+
+
+//My Solution
+
+function countDevelopers(list) {
+    let filtered = list.filter(x => x.continent === 'Europe' && x.language === 'JavaScript');
+    return filtered === [] ? 0 : filtered.length;
+}
+
+//OR
+
+/*
+function countDevelopers(list) {
+  var count = 0;
+  for (let i = 0; i < list.length; i++)  {
+    if (list[i].continent === "Europe" && list[i].language === "JavaScript")  {
+    count++;
+}
+}
+return count;
+}
+*/
