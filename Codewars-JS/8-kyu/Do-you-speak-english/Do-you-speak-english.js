1bd8085d90f90e843407e134a8ffc7dc8e7662c9
Alyssha Lewin <alyssha.lew@gmail.com>
2023-02-26 08:56:14 -0600

    Solution for 2/26/23

---
+++ Codewars-JS/8-kyu/Do-you-speak-english/Do-you-speak-english.js
@@ -0,0 +1,25 @@
+/*
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+*/
+
+
+//My Solution
+
+const spEng = (sentence) => {
+    return sentence.toLowerCase().includes("english")
+}
+
+/*
+Short Solution
+
+
+function spEng(s){
+   return /english/i.test(s)
+}
+*/
