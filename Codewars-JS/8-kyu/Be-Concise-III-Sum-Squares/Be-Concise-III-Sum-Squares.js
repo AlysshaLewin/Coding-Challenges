184e05f7a81027abcb8e03b8593f4745fb9ff749
AlysshaLewin <alyssha.lew@gmail.com>
2023-04-01 20:05:17 -0500

    Solution for 4/1/23

---
+++ Codewars-JS/8-kyu/Be-Concise-III-Sum-Squares/Be-Concise-III-Sum-Squares.js
@@ -0,0 +1,15 @@
+/*
+You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array. For example:
+
+sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+sumSquares([7,3,9,6,5]) === 200
+sumSquares([11,13,15,18,2]) === 843
+Shorten the code such that it meets the requirements.
+*/
+
+
+//Solution
+
+function sumSquares(array) {
+    return array.reduce((a,b) => a + Math.pow(b, 2), 0);
+}
